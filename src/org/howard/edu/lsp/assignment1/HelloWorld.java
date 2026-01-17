e6224398d3ed4bbdfd6371ee57edeee6e3d3913e
Saniya <slicorish05@gmail.com>
2026-01-17 17:09:31 -0500

    Testing a new local change

--- src/org/howard/edu/lsp/assignment1/HelloWorld.java
+++ src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -3,5 +3,6 @@ package org.howard.edu.lsp.assignment1;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("Testing commit");
     }
 }
